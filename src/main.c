dd2703f41c27ac390eca0d204717cbc70d96e090
Poorya Alishah Kamandi <pooryaalishahkamandi14@gmail.com>
2025-08-21 04:53:44 +0330

    integrate the setup event listeners functions

--- src/main.c
+++ src/main.c
@@ -1,14 +1,25 @@
 #include <stdio.h>
 #include "tcp.h"
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>         // close()
-#include <arpa/inet.h>      // sockaddr_in, inet_addr
-#include <fcntl.h>
+
+void on(char *data) {
+
+}
+
+void once(char *data) {
+
+}
+
+void close() {
+
+}
+
+void end() {
+
+}
 
 int main()
 {
-
+    set_event_listeners(once, on, close, end);
     server("localhost", 8080, 1024);
 
 	return 0;
